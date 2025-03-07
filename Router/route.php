9b007a82175823edb7f5d2628768b384a53e511f
Leader-Din <leader.din@student.passerellesnumeriques.org>
2025-03-07 14:08:49 +0700

    pull code

--- Router/route.php
+++ Router/route.php
@@ -4,10 +4,14 @@ require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/WelcomeController.php";
 require_once "Controllers/LoginController.php";
+require_once "Controllers/UserController.php";
 
 
 $route = new Router();
 $route->get("/", [WelcomeController::class, 'welcome']);
 $route->get("/login", [LoginController::class, 'login']);
+
+//route users
+$route->get("/users", [UserController::class, 'index']);
 
 $route->route();
--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -8,7 +8,8 @@
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="/">Home</a>
-                <a class="nav-link" href="/login">Login</a>
+                <a class="nav-link active" aria-current="page" href="/users">User</a>
+                <a class="nav-link" href="">Other</a>
             </div>
         </div>
     </div>
